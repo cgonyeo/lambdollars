a14a9fc6260562cdd9481484172a11a6a6ab664e
Derek Gonyeo <dgonyeo@csh.rit.edu>
2014-11-13 16:03:04 -0500

    postSwagR is a lot saner now

--- Handler/Swag.hs
+++ Handler/Swag.hs
@@ -5,8 +5,8 @@ module Handler.Swag
     ) where
 
 import Import
-import Network.HTTP.Types (status200,status201,status400)
-import Data.Text.Read (decimal)
+import Network.HTTP.Types (status200,status201,status404)
+import Data.Int
 
 getSwagR :: Handler Html
 getSwagR = do
@@ -16,34 +16,31 @@ getSwagR = do
         setTitle "Swag"
         $(widgetFile "swag")
 
+data SwagPurchase = SwagPurchase Int64 Int64
+
+instance FromJSON SwagPurchase where
+    parseJSON (Object o) = SwagPurchase
+                            <$> o .: "uid"
+                            <*> o .: "num"
+
 postSwagR :: Handler ()
 postSwagR = do
-    req <- waiRequest
-    let user = getUser req
-    msid <- lookupPostParam "uid"
-    mnum <- lookupPostParam "num"
-    case (msid,mnum) of
-        (Just tsid,Just tnum) -> do
-            let esid = decimal tsid
-                enum = decimal tnum
-            case (esid,enum) of
-                (Right (sid, _), Right (num, _)) -> do
-                    s <- runDB $ do swagentity <- getBy $ UniqueSwag sid
-                                    case swagentity of
-                                        Just (Entity _ (Swag _ _ _ _ _ c a)) ->
-                                               if a - num >= 0
-                                                   then do let sale = Sale sid
-                                                                      user
-                                                                      num
-                                                                      (c * (fromIntegral num))
-                                                                      False
-                                                           _ <- insert sale
-                                                           _ <- updateWhere [SwagUid ==. sid] [SwagAmount =. (a - num)]
-                                                           return True
-                                                   else return False
-                                        Nothing -> return False
-                    if s
-                        then sendResponseStatus status201 ("CREATED" :: Text)
-                        else sendResponseStatus status200 ("OUT_OF_INVENTORY" :: Text)
-                _ -> sendResponseStatus status400 ("BAD_REQUEST2" :: Text)
-        _ -> sendResponseStatus status400 ("BAD_REQUEST1" :: Text)
+    (SwagPurchase sid num) <- requireJsonBody :: Handler SwagPurchase
+    user <- getUser `fmap` waiRequest
+    (status,s :: Text) <- runDB $ do
+        swag <- get $ SwagKey sid
+        case swag of
+            Just (Swag _ _ _ _ _ c a) ->
+                   if a - num >= 0
+                       then do
+                           let sale = Sale sid
+                                      user
+                                      num
+                                      (c * (fromIntegral num))
+                                      False
+                           _ <- insert sale
+                           _ <- update (SwagKey sid) [SwagAmount =. (a - num)]
+                           return (status201,"SUCCESS")
+                       else return (status200,"OUT_OF_INVENTORY")
+            Nothing -> return (status404,"SWAG_NOT_FOUND")
+    sendResponseStatus status s
